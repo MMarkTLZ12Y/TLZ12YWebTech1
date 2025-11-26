6db10c9d403193872c36c358cc826a0d5cc7f01e
MMark <146880976+MMarkTLZ12Y@users.noreply.github.com>
2025-11-26 12:53:46 +0100

    masodik

--- TLZ12Y_1126/masodik_feladat/4.js
+++ TLZ12Y_1126/masodik_feladat/4.js
@@ -1,54 +1,44 @@
-// 3.js
 $(document).ready(function () {
 
-    // 1. A fejléc szövegéről elhúzott egér – információs ablak
-    $('#fejlec').on('mouseleave', function () {
-        alert('Elhúztad az egeret a fejléc szövegéről.');
+
+    $('#btnBekezdesElrejt').on('click', function () {
+        $('.bekezdes').hide();
     });
 
-    // 2. Első bekezdés "Kattints ide" – elrejti az első bekezdést
-    $('#p1Link').on('click', function (e) {
-        e.preventDefault();
-        $('#p1').hide();
+    $('#btnBekezdesMegjelenit').on('click', function () {
+        $('.bekezdes').show();
     });
 
-    // 3. Második bekezdés "Kattints ide duplán" – elrejti a második bekezdést
-    $('#p2Link').on('dblclick', function (e) {
-        e.preventDefault();
-        $('#p2').hide();
+    $('#btnBekezdesToggle').on('click', function () {
+        $('.bekezdes').toggle();
     });
 
-    // 4. Gomb fölé állva – információs ablak jelenik meg
-    $('#jelentkezesGomb').hover(
-        function () {
-            $('#gombInfo').fadeIn(200);
-        },
-        function () {
-            $('#gombInfo').fadeOut(200);
-        }
-    );
 
-    // 5. Beviteli mezőn fel/le mozgatva az egeret – keret színe változik
-    $('.urlap-mezo').on('mousemove', function (e) {
-        let lastY = $(this).data('lastY') || 0;
 
-        if (e.clientY > lastY) {
-            // lefelé mozgás
-            $(this).css('border-color', 'red');
-        } else if (e.clientY < lastY) {
-            // felfelé mozgás
-            $(this).css('border-color', 'green');
-        }
-
-        $(this).data('lastY', e.clientY);
-    }).on('mouseleave', function () {
-        // ha elhagyjuk a mezőt, visszaáll az eredeti keretszín
-        $(this).css('border-color', '');
-        $(this).removeData('lastY');
+    $('#btnAt01').on('click', function () {
+        $('#urlapDoboz').css('opacity', 0.1);
     });
 
-    // 6. Beviteli mezőn belül kattintunk – hátteret kitölti egy színnel
-    $('.urlap-mezo').on('click', function () {
-        $(this).css('background-color', '#ffffcc'); // halványsárga
+    $('#btnAt05').on('click', function () {
+        $('#urlapDoboz').css('opacity', 0.5);
     });
+
+    $('#btnAt08').on('click', function () {
+        $('#urlapDoboz').css('opacity', 0.8);
+    });
+
+
+
+    $('#btnUrlapElrejt').on('click', function () {
+        $('#urlapDoboz').hide();
+    });
+
+    $('#btnUrlapMegjelenit').on('click', function () {
+        $('#urlapDoboz').show();
+    });
+
+    $('#btnUrlapToggle').on('click', function () {
+        $('#urlapDoboz').toggle();
+    });
+
 });
