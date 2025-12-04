c55f18facf82137a978eb4d126b259e859306e28
MMark <146880976+MMarkTLZ12Y@users.noreply.github.com>
2025-12-04 06:09:55 +0100

    mentes

---
+++ TLZ12Y_1203/2.task/TLZ12Y_1.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    $("#betoltes").on("click", function () {
+
+        $.getJSON("TLZ12Y_orarend.json", function (adat) {
+
+            // cím + telefonok
+            var cim = adat.cim;
+            var telefonok = adat.telefonszam;
+
+            // CSAK 1 óra adata kell -> az első elem
+            var ora = adat.kurzus[0];
+
+            var html = "";
+
+            html += "<h2>MISKOLCI EGYETEM</h2>";
+
+            html += "<p><strong>Cím:</strong> "
+                 + cim.iranyitoszam + " "
+                 + cim.varos + " "
+                 + cim.utca + "</p>";
+
+            html += "<p><strong>Telefonszám:</strong><br>";
+            for (var i = 0; i < telefonok.length; i++) {
+                html += telefonok[i].tipus + ": "
+                     + telefonok[i].szam + "<br>";
+            }
+            html += "</p>";
+
+            html += "<h3>ProgTerv órarend 2025 ősz</h3>";
+
+            html += "<p><strong>Tárgy:</strong> "
+                 + ora.targy + "</p>";
+
+            html += "<p><strong>Időpont:</strong><br>"
+                 + "Nap: " + ora.idopont.nap + "<br>"
+                 + "Tól: " + ora.idopont.tol + "<br>"
+                 + "Ig: " + ora.idopont.ig + "</p>";
+
+            html += "<p><strong>Helyszín:</strong> "
+                 + ora.helyszin + "</p>";
+
+            html += "<p><strong>Oktató:</strong> "
+                 + ora.oktato + "</p>";
+
+            html += "<p><strong>Szak:</strong> "
+                 + ora.szak + "</p>";
+
+            html += "<p><strong>Típus:</strong> "
+                 + ora.tipus + "</p>";
+
+            $("#terulet").html(html);
+
+        }).fail(function () {
+            $("#terulet").html(
+                "<p>Hiba történt a TLZ12Y_orarend.json betöltésekor.</p>"
+            );
+        });
+
+    });
+
+});
